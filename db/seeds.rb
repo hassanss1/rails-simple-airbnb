f8d2949385611b91d121fe101a6785fdd95b2e00
hassanssaade <hassanssaade@gmail.com>
2020-08-07 18:13:27 -0300

    Added 4 flats through seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+flat_names = ['London city Hut', 'In the woods cabin', 'Luxurious apartment in Barcelona', 'Light & Spacious Garden Flat London']
+
+4.times do
+  Flat.create(
+    name: flat_names.sample,
+    address: Faker::Address.street_name,
+    description: Faker::Restaurant.description,
+    price_per_night: rand(75..150),
+    number_of_guests: rand(2..8)
+  )
+end
