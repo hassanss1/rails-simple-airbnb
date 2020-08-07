04e4d1d466062a6fdf1e3c0352a1457f7cdd4791
hassanssaade <hassanssaade@gmail.com>
2020-08-07 18:02:52 -0300

    Adding rails & routes resources

---
+++ db/migrate/20200807210135_create_flats.rb
@@ -0,0 +1,15 @@
+class CreateFlats < ActiveRecord::Migration[6.0]
+  def change
+    create_table :flats do |t|
+      t.string :name
+      t.string :address
+      t.string :description
+      t.integer :price_per_night
+      t.integer :number_of_guests
+      t.integer :badrooms
+      t.integer :bathrooms
+
+      t.timestamps
+    end
+  end
+end
