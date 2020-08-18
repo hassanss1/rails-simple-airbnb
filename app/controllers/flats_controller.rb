2a040d220924586693eaa1dc1cebeedd25481592
hassanssaade <hassanssaade@gmail.com>
2020-08-18 13:23:02 -0300

    added stylesheet and index page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,14 @@
 class FlatsController < ApplicationController
+  before_action :find_flat, only: [:show]
   def index
     @flats = Flat.all
+  end
+
+  def show; end
+
+  private
+
+  def find_flat
+    @flat = Flat.find(params[:id])
   end
 end
