9f44a6ba073e40b2657a8d09c9769ea013dc90b9
hassanssaade <hassanssaade@gmail.com>
2020-08-19 10:25:02 -0300

    added picture migration, edit and delete actions

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :find_flat, only: [:show]
+  before_action :find_flat, only: [:show, :edit, :destroy]
   def index
     @flats = Flat.all
   end
@@ -10,9 +10,26 @@ class FlatsController < ApplicationController
 
   def show; end
 
+  def edit; end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
   private
 
   def find_flat
     @flat = Flat.find(params[:id])
   end
+
+  def flat_params
+    params.require(:flat).permit(:name, :addres, :description, :pricer_per_night, :number_of_guests, :bathrooms, :badrooms)
+  end
+
 end
